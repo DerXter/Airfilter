b5cb1bab9e5c80804dd5a9381746b97c1bd781f2
DerXter <mbayederguene@hotmail.fr>
2019-05-24 17:36:35 +0000

    Suppression

--- connexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-    try{
-        $bdd = new PDO("mysql:host=localhost; dbname=airfliter", "derguene", "ordinateur97");
-    }
-    catch(Exception $e){
-        die('Erreur: '. $e->getMessage());
-    }
